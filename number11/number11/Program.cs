d2c689fc65475d7e463a2c701402c1ca86794d76
Rashif-IN <60098645+Rashif-IN@users.noreply.github.com>
2020-01-20 19:15:30 +0700

    number 11 added separately

---
+++ number11/number11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace number11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            foreach(string X in args)
+            {
+                sum += Convert.ToInt32(X);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
